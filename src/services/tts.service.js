5d21a5be5290ec003f277dbcf9bfc21fccda96ba
juanmapapp-sudo <juanmapapp@gmail.com>
2025-11-19 03:07:15 +0800

    updates

--- src/services/tts.service.js
+++ src/services/tts.service.js
@@ -66,6 +66,7 @@ export async function translateText(
           Accept: "*/*",
           Origin: "https://transword.ai",
           Referer: "https://transword.ai/",
+          Host: "api.transword.ai",
           "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36",
         },
